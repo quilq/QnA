9d2e0371291a49744ddbe2466d9b8a77c38d4e69
Qui <quilq@outlook.com>
2018-08-19 18:11:34 +0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './User/login/login.component';
+import { SignupComponent } from './User/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
